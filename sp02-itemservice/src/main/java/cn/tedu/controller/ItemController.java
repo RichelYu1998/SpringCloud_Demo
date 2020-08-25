e2f220a8be42cabcb73af08b5178e627ad136d3f
980187223@qq.com <Syp950628>
2020-08-25 13:18:15 +0800

    item service 商品服务

---
+++ sp02-itemservice/src/main/java/cn/tedu/controller/ItemController.java
@@ -0,0 +1,35 @@
+package cn.tedu.controller;
+
+import cn.tedu.pojo.Item;
+import cn.tedu.service.ItemService;
+import cn.tedu.web.util.JsonResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+public class ItemController {
+    @Autowired
+    private ItemService itemService;
+
+    @Value("${server.port}")
+    private int port;
+
+    @GetMapping("/{orderId}")
+    public JsonResult<List<Item>> getItems(@PathVariable String orderId) {
+        log.info("server.port="+port+", orderId="+orderId);
+
+        List<Item> items = itemService.getItems(orderId);
+        return JsonResult.ok(items).msg("port="+port);
+    }
+
+    @PostMapping("/decreaseNumber")
+    public JsonResult decreaseNumber(@RequestBody List<Item> items) {
+        itemService.decreaseNumbers(items);
+        return JsonResult.ok();
+    }
+}
