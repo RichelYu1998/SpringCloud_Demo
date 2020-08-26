575839305f42bf3836a80424c0ccfd0df5d6452b
980187223@qq.com <Syp950628>
2020-08-26 11:25:22 +0800

    Ribbon 负载均衡

--- sp06-ribbon/src/main/java/cn/tedu/Sp06RibbonApplication.java
+++ sp06-ribbon/src/main/java/cn/tedu/Sp06RibbonApplication.java
@@ -3,13 +3,14 @@ package cn.tedu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class Sp06RibbonApplication {
-
+    @LoadBalanced //负载均衡注解
     //创建 RestTemplate 实例，并存入 spring 容器
     @Bean
     public RestTemplate getRestTemplate() {
