4e38cd40c0aa1d51daf3c63ec17b8a26df9f565a
980187223@qq.com <Syp950628>
2020-08-26 14:39:16 +0800

    ribbon 重试机制

--- sp02-itemservice/src/main/java/cn/tedu/controller/ItemController.java
+++ sp02-itemservice/src/main/java/cn/tedu/controller/ItemController.java
@@ -1,27 +1,36 @@
 package cn.tedu.controller;
 
+
 import cn.tedu.pojo.Item;
 import cn.tedu.service.ItemService;
 import cn.tedu.web.util.JsonResult;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import java.util.List;
+import java.util.Random;
 
 @Slf4j
 @RestController
 public class ItemController {
-    @Autowired
+    @Resource
     private ItemService itemService;
 
     @Value("${server.port}")
     private int port;
 
     @GetMapping("/{orderId}")
-    public JsonResult<List<Item>> getItems(@PathVariable String orderId) {
+    public JsonResult<List<Item>> getItems(@PathVariable String orderId) throws Exception {
         log.info("server.port="+port+", orderId="+orderId);
+
+        ///--设置随机延迟
+        if(Math.random()<0.6) {
+            long t = new Random().nextInt(5000);
+            log.info("item-service-"+port+" - 暂停 "+t);
+            Thread.sleep(t);
+        }
 
         List<Item> items = itemService.getItems(orderId);
         return JsonResult.ok(items).msg("port="+port);
